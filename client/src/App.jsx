3a8950ae2b1b9aa62e5ad610d940b7a50236993c
Sarthak <gsarthak783@gmail.com>
2024-09-18 21:35:27 +0530

    Speed Insights

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './components/Home';
 import JsonGenerator from './components/JsonGenerator';
 import Header from './components/Header';
-
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 function App() {
+
+  injectSpeedInsights();
+
   return (
     <Router>
       <Header />
-      
       <div className="container mx-auto p-6">
         <Routes>
           <Route path="/" element={<Home />} />
