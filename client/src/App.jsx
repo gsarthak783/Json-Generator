f7b3c45942b34391c7e6f583eb84797f622be0b6
Sarthak <gsarthak783@gmail.com>
2024-09-18 21:32:05 +0530

    Removed Speed Insights

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './components/Home';
 import JsonGenerator from './components/JsonGenerator';
 import Header from './components/Header';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 function App() {
   return (
     <Router>
       <Header />
-      <SpeedInsights/>
+      
       <div className="container mx-auto p-6">
         <Routes>
           <Route path="/" element={<Home />} />
